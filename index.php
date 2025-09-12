b958296354388bd19d053b9ffc702fdac31f1e88
Elnar Käst <elnar.kast@tptlive.ee>
2025-09-12 09:35:16 +0300

    Add if and examples

--- index.php
+++ index.php
@@ -1,27 +1,39 @@
 <?php
+$i = 4;
+if($i > 10){
+    var_dump('bigger');
+} elseif ($i == 10){
+    var_dump('equals');
+} else {
+    var_dump('smaller');
+}
 
-$bool = true && false;
-$bool = FALSE && true;
-$bool = true && true;
-var_dump($bool);
-$bool = true || false;
-$bool = false || false;
-var_dump($bool);
-$bool = !false;
-var_dump($bool);
+$day = (int) date('w', strtoitime('8.09.2025'));
 
-$bool = (true || false && true) && !(!false || false && true);
-var_dump($bool);
+var_dump($day);
 
-$bool = 10 > 4;
-$bool = 10 < 4;
-$bool = 10 < 10;
-$bool = 10 > 10;
-$bool = 10 == 10;
-$bool = 10 == '10';
-$bool = 10 === '10';
-$bool = 10 != '10';
-$bool = 10 !== '10';
-$bool = 10 <= 10;
-$bool = 10 >= 10;
-var_dump($bool);
+switch($day) {
+    case 1:
+        var_dump('Esmaspäev');
+        break;
+    case 2:
+        var_dump('Teisipäe');
+        break;
+    case 3:
+        var_dump('Kolmapäev');
+        break;
+    case 4:
+        var_dump('Neljapäev');
+        break;
+    case 5:
+        var_dump('Reede');
+        break;
+    case 6:
+        var_dump('Laupäev');
+        break;
+    case 7:
+        var_dump('Pühapäev');
+        break;
+    default:
+        var_dump('Imelik päev');
+}
